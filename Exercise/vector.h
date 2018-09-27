dc1919ef1e5161453c88e6e804def35e9afc932d
PerezEnric <enriic19@gmail.com>
2018-09-28 00:00:11 +0200

    vec3 finished

--- Exercise/vector.h
+++ Exercise/vector.h
@@ -59,42 +59,40 @@ public:
 
 		return v1;
 	}
-	bool operator==(Vec3 v1)
+	bool operator==(Vec3 v1) const
 	{
 		return (x == v1.x && y == v1.y && z == v1.z);
 	}
 	void normalize()
 	{
 		Type res;
-		Vec3 vec1;
 
 		res = sqrt((x*x) + y*y + z*z);
 
-		vec1.x = x / res;
-		vec1.y = y / res;
-		vec1.z = z / res;
+		x = x / res;
+		y = y / res;
+		z = z / res;
 
-		cout << vec1.x << ", " << vec1.y << ", " << vec1.z << endl;
 	}
 	void zero()
 	{
-		Vec3 v;
-
-		v.x = 0.0f;
-		v.y = 0.0f;
-		v.z = 0.0f;
-
-		cout << v.x << ", " << v.y << ", " << v.z << endl;
+		x = y = z = 0.0f;
 	}
 	bool is_zero() const
 	{
 		return (x == 0.0f && y == 0.0f && z == 0.0f);
 	}
 
-	Type distance_to(Vec3 &v1) const
+	Type distance_to(Vec3 &v1) 
 	{
 		return sqrt(((x - v1.x) * (x - v1.x)) + ((y - v1.y) * (y - v1.y)) + ((z - v1.z) * (z - v1.z)));
 	}
+
+	Type distance_to_squared(Vec3 &v1) 
+	{
+		return ((x - v1.x) * (x - v1.x)) + ((y - v1.y) * (y - v1.y)) + ((z - v1.z) * (z - v1.z));
+	}
+
 };
 
 typedef Vec3 <float> n_vec;
